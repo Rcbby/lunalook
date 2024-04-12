35582f3076b38c7d92f8c707f9dd69b1578a3e12
nyhl.nython@protonmail.com <robby.kallen@unic.com>
2024-04-12 17:55:17 +0200

    fixed html not showing

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line no-undef
+module.exports = {
+  content: ["./src/**/*.{html,js,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  // eslint-disable-next-line no-undef
+  plugins: [require('daisyui')],
+}
+
