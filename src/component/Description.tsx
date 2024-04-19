09f04cc7a393c3bd9a49aea6ddce7f9aa9c61437
nyhl.nython@protonmail.com <robby.kallen@unic.com>
2024-04-19 18:00:44 +0200

    Fixed so fucking bullshit typescript bug

--- src/component/Description.tsx
+++ src/component/Description.tsx
@@ -3,41 +3,41 @@ import {useEffect, useState} from "react";
 
 const Description = (props: { weatherData: WeatherData | undefined }) => {
 
-    const [uvIndex, setUvIndex] = useState<string>("")
+    const uvIndex = useState<string>("")
 
     useEffect(() => {
-        if (props.weatherData) {
-
-            switch (props.weatherData.currentConditions.uvindex) {
-                case 0:
-                    break;
-                case 1:
-                    break;
-                case 2:
-                    setUvIndex("Kein Schutz erforderlich.");
-                    break;
-                case 3:
-                    setUvIndex("T-Shirt und Sonnenbrille.");
-                    break;
-                case 4:
-                case 5:
-                    setUvIndex("T-Shirt, milde Sonnencreme und Sonnenbrille.");
-                    break;
-                case 6:
-                case 7:
-                case 8:
-                    setUvIndex("Intensive Sonnencreme, Sonnenbrille und Kopfbedeckung.");
-                    break;
-                case 9:
-                case 10:
-                    setUvIndex("Schützen vor der Sonne mit allen verfügbaren Mitteln.");
-                    break;
-                default:
-                    setUvIndex("Es wird dringend empfohlen, sich vor der Sonne zu schützen.");
-                    break;
+            if (props.weatherData) {
+                switch (props.weatherData.currentConditions.uvindex) {
+                    case 0:
+                        break;
+                    case 1:
+                        break;
+                    case 2:
+                        uvIndex[1]("Kein Schutz erforderlich.");
+                        break;
+                    case 3:
+                        uvIndex[1]("T-Shirt und Sonnenbrille.");
+                        break;
+                    case 4:
+                    case 5:
+                        uvIndex[1]("T-Shirt, milde Sonnencreme und Sonnenbrille.");
+                        break;
+                    case 6:
+                    case 7:
+                    case 8:
+                        uvIndex[1]("Intensive Sonnencreme, Sonnenbrille und Kopfbedeckung.");
+                        break;
+                    case 9:
+                    case 10:
+                        uvIndex[1]("Schützen vor der Sonne mit allen verfügbaren Mitteln.");
+                        break;
+                    default:
+                        uvIndex[1]("Es wird dringend empfohlen, sich vor der Sonne zu schützen.");
+                        break;
+                }
             }
         }
-    }, [props.weatherData]);
+        , [props.weatherData, uvIndex])
 
     return (
         <div>
@@ -45,10 +45,11 @@ const Description = (props: { weatherData: WeatherData | undefined }) => {
                 {props.weatherData?.days[0].description}
             </p>
             <h6>
-                {`${uvIndex} Temperatur fühlt sich an wie ${props.weatherData?.currentConditions.feelslike}°C.`}
+                {`${uvIndex[0]} Temperatur fühlt sich an wie ${props.weatherData?.currentConditions.feelslike}°C.`}
             </h6>
         </div>
     );
 }
 
 export default Description;
+
